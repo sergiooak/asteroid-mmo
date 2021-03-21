bd4fa29f66f0d040d1a5f7ead64edcdc75c0b64e
Sergio Carvalho <sergiopcn@gmail.com>
2021-03-21 15:47:07 -0300

    better controls

--- pages/index.vue
+++ pages/index.vue
@@ -1,14 +1,48 @@
 <template>
-  <div
-    class="container h-screen mx-auto text-white"
-  >
+  <div class="container h-screen mx-auto text-white">
     <div class="flex flex-col justify-between w-full h-full py-6">
-      <header class="mb-3">{{player}}</header>
-      <div class="w-full aspect-w-16 aspect-h-9">
-        <canvas width="1600px" height="900px" class="w-full bg-black"></canvas>
-      </div>
+      <header class="mb-3">
+        <div v-if="player">
+          <div class="flex">
+            <button
+          @click="jump()"
+          class="px-6 py-3 ml-6 border border-white"
+        >
+          Anda
+        </button>
+        <button
+          @click="down()"
+          class="px-6 py-3 ml-6 border border-white"
+        >
+          Ré
+        </button>
+        <button
+          @click="rotateLeft()"
+          class="px-6 py-3 ml-6 border border-white"
+        >
+          Girar para esquerda
+        </button>
+        <button
+          @click="rotateRight()"
+          class="px-6 py-3 ml-6 border border-white"
+        >
+          Girar para direira
+        </button>
+          </div>
+        </div>
+      </header>
+      <div id="gameArea" class="w-full aspect-w-16 aspect-h-9"></div>
       <footer>
-        WIP
+        <button @click="addPlayer" class="px-6 py-3 border border-white">
+          Add Players
+        </button>
+        <button
+          @click="removePlayer"
+          class="px-6 py-3 ml-6 border border-white"
+        >
+          Remove Player
+        </button>
+        <span class="ml-6">Player: {{ players.length }}</span>
       </footer>
     </div>
   </div>
@@ -22,94 +56,143 @@ export default {
   name: "AsteroidsMMO",
   data() {
     return {
-      canvas: null,
-      ctx: null,
+      Engine: Matter.Engine,
+      Render: Matter.Render,
+      World: Matter.World,
+      Bodies: Matter.Bodies,
+      Body: Matter.Body,
 
-      player: {
-        x: 0,
-        y: 0,
-        angle: 0,
-        vel: 0,
+      engine: null,
+      render: null,
+
+      canvas: {
+        w: 1600,
+        h: 900,
       },
 
-      playersSizes: {
-        width: 100,
-        height: 100,
-      },
-
-      fps: 30,
+      player: null,
+      players: [],
     };
   },
   mounted() {
     this.start();
+    window.addEventListener("keydown", this.keyHandler, false);
   },
   methods: {
     start() {
-      this.canvas = document.querySelector("canvas");
-      this.ctx = this.canvas.getContext("2d");
+      this.engine = this.Engine.create();
+      this.render = this.Render.create({
+        element: document.querySelector("#gameArea"),
+        engine: this.engine,
+        options: {
+          width: this.canvas.w,
+          height: this.canvas.h,
+          pixelRatio: 1,
+          background: "transparent",
+          wireframeBackground: "transparent",
+          enabled: true,
+          wireframes: false,
+          showVelocity: true,
+          showAngleIndicator: false,
+          showCollisions: true,
+        },
+      });
 
-      // Keys listeners
-      window.addEventListener("keydown", this.keyHandler, false);
+      this.engine.world.gravity.y = 0;
 
-      // Reset player in the middle
-      this.player.x = this.canvas.width / 2;
-      this.player.y = this.canvas.height / 2;
+      let walls = [
+        this.Bodies.rectangle(0 - 25, this.canvas.h / 2, 50, this.canvas.h, {
+          isStatic: true,
+        }),
+        this.Bodies.rectangle(
+          this.canvas.w + 25,
+          this.canvas.h / 2,
+          50,
+          this.canvas.h,
+          { isStatic: true }
+        ),
+        this.Bodies.rectangle(
+          this.canvas.w / 2,
+          this.canvas.h + 25,
+          this.canvas.w,
+          50,
+          { isStatic: true }
+        ),
+        this.Bodies.rectangle(this.canvas.w / 2, 0 - 25, this.canvas.w, 50, {
+          isStatic: true,
+        }),
+      ];
 
-      this.draw();
+      // add all of the bodies to the world
+      this.World.add(this.engine.world, walls);
+
+      // run the engine
+      this.Engine.run(this.engine);
+
+      // run the renderer
+      this.Render.run(this.render);
+
+      this.addPlayer(true);
     },
 
-    erase() {
-      this.ctx.fillStyle = "#FFFFFF";
-      this.ctx.fillRect(0, 0, this.canvas.width, this.canvas.height);
+    jump() {
+      this.Body.setVelocity(this.player, {
+        x: Math.cos(this.player.angle - 1.5) * 5,
+        y: Math.sin(this.player.angle - 1.5) * 5,
+      });
+    },
+    down() {
+      this.Body.setVelocity(this.player, {
+        x: Math.cos(this.player.angle - 1.5) * -5,
+        y: Math.sin(this.player.angle - 1.5) * -5,
+      });
+    },
+    rotateLeft() {
+      if (this.player.angularVelocity > -0.1) {
+        this.Body.setAngularVelocity(
+          this.player,
+          this.player.angularVelocity - 0.005
+        );
+      }
+    },
+    rotateRight() {
+      if (this.player.angularVelocity < 0.1) {
+        this.Body.setAngularVelocity(
+          this.player,
+          this.player.angularVelocity + 0.005
+        );
+      }
     },
 
-    async draw() {
-      this.erase();
+    async addPlayer(me) {
+      let i = 0;
+      while (i < 1) {
+        const map = (value, x1, y1, x2, y2) =>
+          ((value - x1) * (y2 - x2)) / (y1 - x1) + x2;
+        let x = map(Math.random(), 0, 1, 0 + 20, this.canvas.w - 20);
+        let box = this.Bodies.rectangle(x, this.canvas.h / 2, 35, 51, {
+          render: {
+            sprite: {
+              texture: "/img/player.png",
+            },
+          },
+        });
+        if (me) {
+          this.player = box;
+        }
+        this.players.push(box);
+        this.World.add(this.engine.world, box);
+        await wait(50);
+        i++;
+      }
+    },
 
-      // Desenha player
-      this.calcPlayer();
-      this.renderPlayer(
-        this.player.x,
-        this.player.y,
-        this.playersSizes.width,
-        this.playersSizes.height
+    removePlayer() {
+      Matter.Composite.remove(
+        this.engine.world,
+        this.players[this.players.length - 1]
       );
-
-      await wait(1000 / this.fps);
-      this.draw();
-    },
-
-    // Moviments
-    speedUp() {
-      this.player.vel++;
-    },
-
-    speedDown() {
-      this.player.vel--;
-    },
-
-    angleRight() {
-      this.player.angle+= 5;
-    },
-
-    angleLeft() {
-      this.player.angle-= 5;
-    },
-
-    // Rederer
-    renderPlayer(x, y, width, height) {
-      this.ctx.save();
-      this.ctx.translate(x, y);
-      this.ctx.rotate((this.player.angle * Math.PI) / 180);
-      this.ctx.fillStyle = "#FF0000";
-      this.ctx.fillRect(0 - width / 2, 0 - height / 2, width, height);
-      this.ctx.restore();
-      // this.ctx.rotate(0)
-    },
-
-    // Calc positions
-    calcPlayer(){
-
+      this.players.pop();
     },
 
     // Utils
@@ -117,16 +200,16 @@ export default {
       let code = e.keyCode;
       switch (code) {
         case 37:
-          this.angleLeft();
+          this.rotateLeft();
           break; //Left key
         case 38:
-          this.speedUp();
+          this.jump();
           break; //Up key
         case 39:
-          this.angleRight();
+          this.rotateRight();
           break; //Right key
         case 40:
-          this.speedDown();
+          this.down();
           break; //Down key
       }
     },
